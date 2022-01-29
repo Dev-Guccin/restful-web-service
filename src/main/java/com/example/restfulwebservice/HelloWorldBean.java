1a55ebe6e878ed89b555cb7b204c2ab21380cfad
Dev-Guccin <ghks2047@naver.com>
2022-01-29 23:49:08 +0900

    first commit

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// lombok => 빈 클래스를 만들때 get,set,constructor를 자동으로 생성해준다.
+@Data // 이 선언만으로 HelloWorldBean에 대한 생성자, 세터,게터 전부 생성됨.
+@AllArgsConstructor // 알아서 초기화 해준다. 변수에 대한 생성자 생성
+@NoArgsConstructor // 매개변수가 없는 생성자를 생성
+public class HelloWorldBean {
+    private String message;
+}
---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-world (endpoint)
+    // @RequestMapping()
+    @GetMapping(path="/hello-world") // 어떤 메소드인지 명시할 수 있다.
+    public String helloWorld(){
+        return "hello";
+    }
+
+    @GetMapping(path="/hello-world-bean") // 어떤 메소드인지 명시할 수 있다.
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World"); // 객체 반환의 경우 JSON으로 바꿔서 리턴한다.
+    }
+
+    @GetMapping(path="/hello-world-bean/path-variable/{name}") // 어떤 메소드인지 명시할 수 있다.
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s",name)); // 객체 반환의 경우 JSON으로 바꿔서 리턴한다.
+    }
+
+
+
+
+
+}
