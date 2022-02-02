b4f5aa2cac98fa0f8aff6a8f3a07f4139a043698
Dev-Guccin <ghks2047@naver.com>
2022-02-02 16:18:57 +0900

    feat: Construct Exception, AOP

---
+++ src/main/java/com/example/restfulwebservice/exception/CustomizedReponseEntityExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.restfulwebservice.exception;
+
+import com.example.restfulwebservice.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice // 모든 컨트롤러 실행전에 @ControllerAdvice로 선언된 Bean이 먼저 실행된다.
+public class CustomizedReponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class) // Exceptions 발생시 이렇게 작동함.
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class) // Exceptions 발생시 이렇게 작동함.
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+}
