435dbd2baf62bcad78860af986b581960f31fe71
Dev-Guccin <ghks2047@naver.com>
2022-02-15 19:19:36 +0900

    feat: Construct Internationalized Message

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.example.restfulwebservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     // GET
     // /hello-world (endpoint)
     // @RequestMapping()
@@ -22,5 +31,10 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world-bean/path-variable/{name}") // 어떤 메소드인지 명시할 수 있다.
     public HelloWorldBean helloWorldBean(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s",name)); // 객체 반환의 경우 JSON으로 바꿔서 리턴한다.
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
